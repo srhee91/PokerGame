45ec7bd744fe0585008045924dcd71deaef637a1
srhee91 <leesy579@gmail.com>
2014-02-09 01:46:02 -0500

    proj

---
+++ 04. Project/src/Poker.java
@@ -0,0 +1,23 @@
+import GUI.GUI;
+
+/**
+ * 
+ */
+
+/**
+ * @author Sang
+ *
+ */
+public class Poker {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		GUI gui = new GUI();
+		new Thread(gui).start();
+		
+	}
+
+}

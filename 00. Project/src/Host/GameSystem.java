cb540d03b318d8eb5cffe7b3cae363dcc5ee93cd
jeongb <jeongb@purdue.edu>
2014-02-23 21:12:50 -0500

    DECK CLASS

--- 00. Project/src/Host/GameSystem.java
+++ 00. Project/src/Host/GameSystem.java
@@ -2,5 +2,15 @@ package Host;
 
 //library of game algorithm
 public class GameSystem {
-
+	int firstcard;
+	int secondcard; 
+	
+	public void createDeck(){
+		
+	}
+	
+	public int campareHand(int i, int j){
+		int score;
+		return score;
+	}
 }
---
+++ 00. Project/src/Host/deck.java
@@ -0,0 +1,11 @@
+package Host;
+
+public class deck {
+	 public final static int kinds = 13;
+	 public final static int SPADES = 0;
+	 public final static int HEART = 1; 
+	 public final static int CLOVER = 2;
+	 public final static int DIAMOND = 3; 
+	 
+	
+}

289e5b2cdc4d46aadcf83cb6678739c215388098
choi258 <whitecloudz88@gmail.com>
2014-02-27 12:25:38 +0900

    calculateRank Flush&Straight

--- 00. Project/src/Host/CalculateRank.java
+++ 00. Project/src/Host/CalculateRank.java
@@ -1,11 +1,11 @@
 package Host;
-
 import java.util.*;
 
 //For debugging Goto Deck.java
 public class CalculateRank {
 	public static Card[][] merge_arr;
 	public static final int PLAYER_MAX=3;		//temporary exist for player number
+	
 	
 	public CalculateRank(){
 		merge_arr=new Card[PLAYER_MAX][7];//7 = player cards (2) + flop cards (5)
@@ -33,6 +33,7 @@ public class CalculateRank {
 
 	public void findBestHand(){			//Will find the best Rank.
 		
+		
 	}
 	
 	public int compareHands(Card[]play){	//overal method that will decide the final rank
@@ -47,8 +48,9 @@ public class CalculateRank {
 		int heart_num=0;				//HEART = 2;
 		int diamond_num=0;				//DIAMOND = 3;
 		int spade_num=0;				//SPADE = 4;
+		int royal_flush_helper=0;		//helper variable
 		
-		
+
 		for(int i=0;i<7;i++){
 			switch(play[i].getKind()){
 			case 1:	clover_num++;
@@ -61,42 +63,98 @@ public class CalculateRank {
 					break;
 			}
 		}
-		//for debugging goto Deck.java
-		//System.out.println("clover:"+clover_num+" "+"heart:"+heart_num+" "+"diamond:"+diamond_num+" "+"spade"+spade_num); 
 		
-		if(clover_num>=5||heart_num>=5||diamond_num>=5||spade_num>=5){
+		//for debugging goto Deck.java
+		System.out.println("clover:"+clover_num+" "+"heart:"+heart_num+" "+"diamond:"+diamond_num+" "+"spade"+spade_num); 
+		
+		if(clover_num>=5){
 			isFlush=1;
+		}else if(heart_num>=5){
+			isFlush=2;
+		}else if(diamond_num>=5){
+			isFlush=3;
+		}else if(spade_num>=5){
+			isFlush=4;
+		}
+		
+		if(isFlush!=0){
+				
+			for(int i=0;i<7;i++){			//checks for royal_flush
+				if(play[i].getKind()==isFlush&&(play[i].getNumber()==1
+					||play[i].getNumber()==10||play[i].getNumber()==11
+					||play[i].getNumber()==12||play[i].getNumber()==13)){
+					royal_flush_helper++;
+				}
+			}
+			
+			if(royal_flush_helper==5){			//is royal flush
+				isFlush=Rank.ROYAL_STRAIGHT_FLUSH;
+			}
+			
+			if(isStraight(play)!=null){			//is straight flush
+				isFlush=Rank.STRAIGHT_FLUSH;
+			}
 		}
 		
 		return isFlush; 
 	}
-	public int isStraight(Card[]play){
+	public int[] isStraight(Card[]play){
 		 int isStraight=0;		
 		    int temp[]=new int[7];			//temporary array that will hold 1 set of 7 cards
 		    int count=0;
 		        
 		    for(int i=0;i<7;i++){
-		        //temp[i]=play[i].getNumber();
+		        temp[i]=play[i].getNumber();
 		    }
 		    
 		    Arrays.sort(temp); 			//sort
-		    //System.out.println(Arrays.toString(temp)); for debugging
+		 //  System.out.println(Arrays.toString(temp)); //for debugging
 
-		    for(int i=0;i<3;i++){
+		    for(int i=2; i>=0;i--){
 		        count=0;
-		        int k=i+4;
-		        for(int j=i;j<k;j++){
-		            if (temp[j] + 1 == temp[j + 1]) {
-		                count++;
-		            }else if (temp[j] == temp[j+1])   k++;
-		            if (k > 6) break;
+		        int k=i;
+		        int straight[] = new int[5];
+		        straight[count++] = temp[i+4];
+		        for(int j=i+4;j>k;j--){
+		            if (temp[j-1] + 1 == temp[j]) {
+		                straight[count++] = temp[j-1];
+		            }else if (temp[j-1] == temp[j]) {
+		            	k--;
+		            }
+		            
+		            if (k < 0) break;
 		        }
-		        if(count==4){
-		        	isStraight=1;
-		        	break;
+		        if(count==5){
+		        	System.out.println(Arrays.toString(straight)); 
+		        	return straight;
 		        }
 		    }
-		    return isStraight;
+		    
+		    return null;
 	}
+	public int[] findPair(Card[]play){
+		
+		return null;
+	}
+
+    private static int getMostPopularElement(int[] a){
+
+        int maxElementIndex = getArrayMaximumElementIndex(a); 
+        int[] b= new int[a[maxElementIndex]+1];
+        for(int i = 0; i<a.length;i++){
+               ++b[a[i]];
+        }
+        return getArrayMaximumElementIndex(b);
+    }
+
+    private static int getArrayMaximumElementIndex(int[] a) {
+        int maxElementIndex = 0;
+        for(int i = 1;i<a.length;i++){
+            if(a[i]>=a[maxElementIndex]){
+                maxElementIndex = i;
+            }
+        }
+        return maxElementIndex;
+    }
 	
 }

fc457f91cec8e8147095db0f86efe856cb0cf3fc
choi258 <whitecloudz88@gmail.com>
2014-04-14 09:54:12 +0900

    debug_4/13

--- 00. Project/src/Host/Test/TestRank.java
+++ 00. Project/src/Host/Test/TestRank.java
@@ -122,23 +122,23 @@ public class TestRank {
 	public static void straight_check(Rank calrank){
 		System.out.println("Testing Straight");
 		Card[]flop=new Card[5];
-		flop[0]=new Card(4,7);
-		flop[1]=new Card(3,7);
-		flop[2]=new Card(1,7);
-		flop[3]=new Card(2,10);
-		flop[4]=new Card(1,10);
+		flop[0]=new Card(1,1);
+		flop[1]=new Card(2,2);
+		flop[2]=new Card(3,3);
+		flop[3]=new Card(4,4);
+		flop[4]=new Card(1,5);
 		Card[][]hand=new Card[8][2];
-		hand[0][0]=new Card(1,10);
-		hand[0][1]=new Card(1,2);
+		hand[0][0]=new Card(6,10);
+		hand[0][1]=new Card(6,2);
 		hand[1]=null;
-		hand[2][0]=new Card(1,1);
-		hand[2][1]=new Card(3,4);
-		hand[3][0]=new Card(1,2);
-		hand[3][1]=new Card(2,5);
-		hand[4][0]=new Card(4,1);
-		hand[4][1]=new Card(1,5);
-		hand[5][0]=new Card(4,1);
-		hand[5][1]=new Card(3,5);
+		hand[2][0]=new Card(1,6);
+		hand[2][1]=new Card(2,7);
+		hand[3][0]=new Card(3,6);
+		hand[3][1]=new Card(3,6);
+		hand[4][0]=new Card(1,7);
+		hand[4][1]=new Card(2,7);
+		hand[5][0]=new Card(3,1);
+		hand[5][1]=new Card(4,5);
 		hand[6]=null;
 		hand[7]=null;
 		

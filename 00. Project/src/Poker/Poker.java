4f4379315ba0cc238817f7c1bd352fd2a8f4cc02
wangyix <wangyix@gmail.com>
2014-04-22 23:26:24 -0400

    ffefe

--- 00. Project/src/Poker/Poker.java
+++ 00. Project/src/Poker/Poker.java
@@ -15,6 +15,7 @@ public class Poker {
 			
 			AppGameContainer app = new AppGameContainer(new GUI("Poker"));
 			app.setDisplayMode(1000, 600, false);
+			app.setIcon(GUI.RESOURCES_PATH+"chip30.png");
 			//app.setVSync(true);
 			app.setTargetFrameRate(60);
 			app.start();
